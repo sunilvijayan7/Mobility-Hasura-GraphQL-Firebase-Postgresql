7de3053e582dbf8a58309cd02c6f5b7dcfd3b212
Sunil Vijayan <svijay7487@gmail.com>
2021-05-11 00:23:07 +0400

    Removing the Old ones

--- mobility/android/app/src/main/kotlin/com/example/mobility/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mobility
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mobilityadmin/android/app/src/main/kotlin/com/example/mobilityadmin/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mobilityadmin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
